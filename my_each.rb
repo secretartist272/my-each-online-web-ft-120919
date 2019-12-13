710295b8ce9ddaf91b5c92762f7c3ee070bdae47
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-13 16:42:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,27 @@
+require_relative 'spec_helper'
+
+
 def my_each # put argument(s) here
-  # code here
+  # iterate through array
+ # "yeild" each element to a code block
+ #  were going to do something in the block
+ # after we iterate, we should return original array
+ index = 0
+ while index < array.length 
+    yield(array[index], index)
+    # index += 1
+    index = index + 1
+  end
+  
 end
+
+  array =["Bob", "Sarah", "Tim"]
+
+  my_each(array) do |name, index|
+   puts "#{index}, #{name}"  
+  
+  end
+
+# array.each do |name|
+#   puts name
+# end # original array
