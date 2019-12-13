308777af91eb2740f0267e4da9fd021fe743d5d8
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-13 17:17:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  # iterate through array
+  # "yeild" each element to a code block
+   #  were going to do something in the block
+   # after we iterate, we should return original array
+  index = 0
+   while index < array.length 
+    yield(array[index])
+    # index += 1
+    index = index + 1
+  end
+  return array
 end
